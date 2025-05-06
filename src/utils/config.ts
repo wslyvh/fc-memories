daa188cdc2eb2f6aae0f19aec779e279793c6aec
wslyvh <wslyvh@users.noreply.github.com>
2025-05-06 09:00:12 +0200

    domain

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 export const APP_EMOJI = "💫";
 export const APP_NAME = "Memories";
 export const APP_DESCRIPTION = "Look back at your memories on Farcaster";
-export const APP_URL = "https://fc-memories.vercel.app";
+export const APP_URL = "https://memories.nexth.dev";
 
 export const SOCIAL_TWITTER = "wslyvh";
 export const SOCIAL_GITHUB = "wslyvh/fc-memories";
